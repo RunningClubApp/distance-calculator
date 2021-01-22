6f6179285e984fae154607afe3df733580ccc5d1
Tom Croasdale <tecroasdale@gmail.com>
2021-01-22 17:45:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /**
+   * Calculates the distance between 2 lat/lng objects
+   * @param {Object} one - The first { lat: x, lng: y } coordinate
+   * @param {Object} two - The second { lat: x, lng: y } coordinate
+   * @returns The distance in metres between one and two
+   */
+  DistanceBetweenTwoPoints: (one, two) => {
+    const R = 6371e3 // metres
+    const φ1 = one.lat * Math.PI / 180 // φ, λ in radians
+    const φ2 = two.lat * Math.PI / 180
+    const Δφ = (two.lat - one.lat) * Math.PI / 180
+    const Δλ = (two.lng - one.lng) * Math.PI / 180
+
+    const a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+              Math.cos(φ1) * Math.cos(φ2) *
+              Math.sin(Δλ / 2) * Math.sin(Δλ / 2)
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a))
+
+    const d = R * c // in metres
+    return d
+  },
+  /**
+   * Calculates the distance of a path
+   * @param {Array} path - An in order list of {lat, lng} objects
+   * @returns The distance covered by the path
+   */
+  DistanceOfPath: (path) => {
+    if (path.length === 2) {
+      return module.exports.DistanceBetweenTwoPoints(path[0], path[1])
+    } else if (path.length > 2) {
+      const nextPath = path.splice(0, 1)
+
+      return module.exports.DistanceBetweenTwoPoints(path[0], path[1]) +
+              module.exports.DistanceBetweenTwoPoints(nextPath)
+    } else {
+      return 0
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Description
+This package is used to calculate distances from lat/lng coordinate systems.
